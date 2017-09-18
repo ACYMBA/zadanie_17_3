e10871208ee93c0ecab8977105cfa19ec452e85b
albert <albert.cymbaluk@gmail.com>
2017-09-18 11:27:54 +0200

    new

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var stringifyFile = '';
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function(req,res) {
+		fs.readFile('./test.json', 'utf8', function(err, data) {
+		    if (err) throw err;
+		    fileContent = data
+		    res.send(data);
+		});
+
+	});
+
+
+
+ app.post('/updateNote/:note', function (req, res) {
+ 	    console.log('Otrzymałem żądanie POST do strony głównej');
+   		res.send('Hello POST!');
+		fs.writeFile('./test.json', stringifyFile, function(req, res, next) {
+	    if (err) throw err;
+	    console.log('file updated');
+		});
+	});
+
+app.use(function (req, res, next) {
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
+});
+
+
+app.listen(3000);
